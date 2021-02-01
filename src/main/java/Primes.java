335261cf762f1a1d6ef64e77f551e66cd8d3ae70
Vincent Carlton <vincent.236568@nv.ccsd.net>
2021-02-01 19:34:20 +0000

    everything

--- src/main/java/Primes.java
+++ src/main/java/Primes.java
@@ -1,19 +1,36 @@
 public class Primes {
     public static int countFactors(int n){
         int factors = 0;
-        // replace with your code
+        
+        for(int divNum = 1; divNum<=n; divNum++ ){
+            if(n%divNum==0){
+                factors++;
+            }
+            
+        }
         return factors;
     }
 
     public static boolean isPrime(int n){
         boolean prime = true;
-        // replace with your code
+        int root = (int) Math.sqrt(n);
+        for(int divNum = 2; divNum<=root; divNum++ ){
+            if(n%divNum==0){
+                prime = false;
+            }
+            
+        }
         return prime;
     }
 
     public static int countPrimes(int n){
         int numPrimes = 0;
-        // replace with your code
+        for(int i=2;i<n;i++){
+          if(isPrime(i)){
+            numPrimes++;
+           
+          }
+        }
         return numPrimes;
     }
 }
